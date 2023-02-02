1bc4fd86b3126b83f6fc76c62b1a5eaa843ce0d2
NadiaVoloshyna <dnipronadiya@gmail.com>
2023-02-02 16:54:47 +0000

    updated request, modified the program

--- dataset.py
+++ dataset.py
@@ -1,22 +1,23 @@
 """
 Program: dataset.py
 Author: Nadiia Voloshyna
-1. Inputs
-   set of data
-2. Computations
-   frequency for non-numeric values
-   mean for numeric values 
-3. Outputs
-   the resultant data in a tabular format
 """
-
 import pandas as pd
+import math
 
+hero = input("Enter the hero's name: ")
 heroes = pd.read_csv("C:\ABERTAY\Voloshyna_Week5-assignment\SuperHeroes.csv", sep=';')
 df = heroes.copy()
-dfLength = len(heroes)
 
-# Group by the column
+# Check if the hero's name exists in the dataset and get the hero's data
+names = df.groupby(df.Name)
+try:
+   heroProfile = names.get_group(hero)
+   heroAlignment = heroProfile["Alignment"].values[0]
+except KeyError:
+   print("Error: the hero with this name does not exist!")
+
+# Get dataFrames with good and bad heroes
 grouped = df.groupby(df.Alignment)
 df_bad = grouped.get_group("bad")
 df_good = grouped.get_group("good")
@@ -24,38 +25,43 @@ df_neutral = grouped.get_group("neutral")
 df_unknown = grouped.get_group("unknown")
 
 # Check the created dataframes
-df_badLength = len(df_bad)
-df_goodLength = len(df_good)
-df_neutralLength = len(df_neutral)
-df_unknownLength = len(df_unknown)
-
+dfLength, df_badLength, df_goodLength, df_neutralLength, df_unknownLength = len(heroes), len(df_bad), len(df_good), len(df_neutral), len(df_unknown)
 if df_badLength + df_goodLength != dfLength - df_neutralLength - df_unknownLength:
     print("Error: the data set was split incorrectly!")
 
+# Calculate frequency for non-numeric values and a mean for numeric values. Make a list of characteristics
 def handleDataframe(dataFrame):
-  tabName = dataFrame["Alignment"].value_counts().idxmax()
   res = []
   header = dataFrame.columns.values
   for item in header:
     if item == 'Gender' or item == 'Eye color' or item == 'Race' or item == 'Hair color' or item == 'Publisher' or item == 'Skin color':
         res.append(dataFrame[item].value_counts().idxmax())
     elif item == 'Height' or item == 'Weight':
-        res.append(round(dataFrame[item].describe().loc['mean'], 2))
+      value = round(dataFrame[item].describe().loc['mean'], 2)
+      if math.isnan(value) == True:
+         res.append('X')
+      else:
+         res.append(value)
+  return res
 
-  print("An average", tabName, "guy portrait:")
+# Display the header and values for the table of comparison
+def compare(dataFrame, heroProfile):
+   print("%29s%22s" % (hero, f"Average {heroAlignment}"))
+   print("-----------------------------------------------------")
 
-  # Display the header and values for the table
-  items = ["Gender: ", "Eye color: ", "Race: ", "Hair color: ", "Height: ", "Publisher: ", "Skin color: ", "Weight: "]
-  for item, name in zip(items, res):
+   list = handleDataframe(heroProfile)
+   res = handleDataframe(dataFrame)
+   items = ["Gender: ", "Eye color: ", "Race: ", "Hair color: ", "Height: ", "Publisher: ", "Skin color: ", "Weight: "]
+   for item, el, name in zip(items, list, res):
         if type(name) == 'float': 
-         print("%-12s%20d" % (item, name))
+         print("%-12s%20d%20d" % (item, el, name))
         else:
-         print("%-12s%20s" % (item, name))
+         print("%-12s%20s%20s" % (item, el, name))
+   print("-----------------------------------------------------")
 
-print("-----------------------------------")
-handleDataframe(df_bad)
-print("-----------------------------------")
-handleDataframe(df_good)
-print("-----------------------------------")
+if heroAlignment == 'bad':
+   compare(df_bad, heroProfile)
+else:
+   compare(df_good, heroProfile)
 
 
